a87f98321488a21ca44decb5570a0c28a59bbaa0
Markus Sekhon <markus.sekhon@pm.me>
2024-07-07 01:20:23 -0700

    got save and load files

--- src/main.py
+++ src/main.py
@@ -1,17 +1,41 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import filedialog
 
-def main():
-    root = tk.Tk()
-    root.title("noted")
+class NotedApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("noted")
+        self.root.geometry("400x400")
 
-    mainframe = ttk.Frame(root)
-    mainframe.grid(row=0,column=0)
-        
-    text = tk.Text(mainframe, bg="black", fg="white")
-    text.grid(row=0,column=0)
+        menuframe = tk.Frame(self.root, bg="lightgrey", height=40)
+        menuframe.pack(side=tk.TOP, fill=tk.X)
 
-    root.mainloop()
+        save = tk.Button(menuframe, text="save", command=self.saveNote)
+        save.pack(side=tk.LEFT, padx=5, pady=5)
+        load = tk.Button(menuframe, text="load", command=self.loadNote)
+        load.pack(side=tk.LEFT, padx=4, pady=5)
+
+        self.text = tk.Text(self.root, wrap=tk.WORD, bg="black", fg="white")
+        self.text.pack(expand=True,fill=tk.BOTH)
+
+
+
+    def saveNote(self):
+        filepath = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
+
+        if filepath:
+            with open(filepath, "w") as file:
+                file.write(self.text.get(1.0, tk.END))
+
+    def loadNote(self):
+        filepath = filedialog.askopenfilename(filetypes=[("Text files","*.txt")])
+
+        if filepath:
+            with open(filepath, "r") as file:
+                self.text.delete(1.0, tk.END)
+                self.text.insert(tk.END, file.read())
 
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    app = NotedApp(root)
+    root.mainloop()
